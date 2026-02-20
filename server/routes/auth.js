46c99528c6e04c87eba3b99acb89810ca176f60b
KasenaM <kasenamelvin@gmail.com>
2026-02-20 03:04:47 +0300

    Fix case sensitivity for User mode

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 const verifyRecaptcha = require("../middleware/verifyRecaptcha");
 const authMiddleware = require("../middleware/authMiddleware");
 
